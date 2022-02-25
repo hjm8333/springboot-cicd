bd00e96cbbfa363e48cd12a070307b86ee64d145
hjm8333 <hjm8333@naver.com>
2022-02-25 10:11:24 +0900

    FIRST commit

---
+++ src/main/java/com/sptest/sptest/SptestApplication.java
@@ -0,0 +1,13 @@
+package com.sptest.sptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SptestApplication.class, args);
+	}
+
+}
